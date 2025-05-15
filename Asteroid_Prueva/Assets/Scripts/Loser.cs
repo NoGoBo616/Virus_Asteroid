a7981701de5780127694cd79bd2530c1bac4ebea
Izquierdo768 <jizquierdoolmos@gmail.com>
2025-05-15 09:21:31 +0200

    puntos menu

---
+++ Asteroid_Prueva/Assets/Scripts/Loser.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //title.text = "Ha perdido "+ StaticLoser.loserName + "\nHa ganado " + StaticLoser.winnerName;
+    }
+
+    
+}
