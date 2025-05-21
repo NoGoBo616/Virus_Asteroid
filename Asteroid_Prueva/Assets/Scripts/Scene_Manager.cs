e704ab4c4266aa1005e520f7c48e70998776d501
NoGoBo616 <noegoterrisborras@gmail.com>
2025-05-21 21:57:36 +0200

    awsdfghjkll yaaaaaaaaaaa como este es el last comit pongo algo random... almendra

--- Asteroid_Prueva/Assets/Scripts/Scene_Manager.cs
+++ Asteroid_Prueva/Assets/Scripts/Scene_Manager.cs
@@ -21,7 +21,6 @@ public class Scene_Manager : MonoBehaviour
         map = Random.Range(1, 4);
         SceneManager.LoadScene(2);
         crono.gameObject.SetActive(true);
-        
     }
 
     public void ChangeMap_GameInfiniteMode()
@@ -38,7 +37,6 @@ public class Scene_Manager : MonoBehaviour
 
     public void ChangeMap_GameOver()
     {
-        
         crono.gameObject.SetActive(false);
         SceneManager.LoadScene(3);
         Destroy(gameObject);
@@ -49,10 +47,7 @@ public class Scene_Manager : MonoBehaviour
         if (GlobalAudioManager.Instance != null)
         {
             GlobalAudioManager.Instance.PlaySFX(0);
-
-
         }
-
         crono.gameObject.SetActive(false);
         SceneManager.LoadScene(1);
         Destroy(gameObject);
@@ -63,8 +58,6 @@ public class Scene_Manager : MonoBehaviour
         if (GlobalAudioManager.Instance != null)
         {
             GlobalAudioManager.Instance.PlaySFX(0);
-
-
         }
         crono.gameObject.SetActive(false);
         SceneManager.LoadScene(4);
@@ -75,8 +68,6 @@ public class Scene_Manager : MonoBehaviour
         if (GlobalAudioManager.Instance != null)
         {
             GlobalAudioManager.Instance.PlaySFX(0);
-
-
         }
         Application.Quit();
     }
