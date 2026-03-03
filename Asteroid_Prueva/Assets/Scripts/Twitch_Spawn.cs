526ac6754a3d52801013b283b32f48dcadeb9ea5
NoGoBo616 <noegoterrisborras@gmail.com>
2026-03-03 17:01:42 +0100

    twitch script

---
+++ Asteroid_Prueva/Assets/Scripts/Twitch_Spawn.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System;
+using TMPro;
+
+public class Twitch_Spawn : MonoBehaviour
+{
+    public string channelName = "tucanal";
+    public TMP_InputField channelInput;
+
+    [Header("Prefab a instanciar")]
+    public GameObject prefabNPC;
+
+    private TcpClient twitchClient;
+    private StreamReader reader;
+    private StreamWriter writer;
+    private Thread twitchThread;
+    private bool running;
+
+    private string messageFromChat = null;
+
+    public void Iniciar()
+    {
+        channelName = channelInput.text.ToLower();
+        running = true;
+        twitchThread = new Thread(ConnectToTwitch);
+        twitchThread.IsBackground = true;
+        twitchThread.Start();
+    }
+
+    void Update()
+    {
+
+        if (!string.IsNullOrEmpty(messageFromChat))
+        {
+            string msg = messageFromChat.ToLower();
+            messageFromChat = null;
+
+            if (msg.Contains("!npc"))
+            {
+                SpawnNPC();
+            }
+        }
+    }
+
+    public void ConnectToTwitch()
+    {
+        try
+        {
+            twitchClient = new TcpClient("irc.chat.twitch.tv", 6667);
+            reader = new StreamReader(twitchClient.GetStream());
+            writer = new StreamWriter(twitchClient.GetStream());
+
+            writer.WriteLine("PASS SCHMOOPIIE");
+            writer.WriteLine("NICK justinfan12345");
+            writer.WriteLine("JOIN #" + channelName.ToLower());
+            writer.Flush();
+
+            Debug.Log("Conectado al chat de Twitch #" + channelName);
+
+            while (running)
+            {
+                if (twitchClient.Available > 0)
+                {
+                    string line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        if (line.StartsWith("PING"))
+                        {
+                            writer.WriteLine("PONG :tmi.twitch.tv");
+                            writer.Flush();
+                        }
+                        else
+                        {
+                            if (line.Contains("PRIVMSG"))
+                            {
+                                int splitPoint = line.IndexOf("!", 1);
+                                string user = line.Substring(1, splitPoint - 1);
+
+                                int msgIndex = line.IndexOf(":", 1);
+                                string message = line.Substring(msgIndex + 1);
+
+                                Debug.Log(user + ": " + message);
+                                messageFromChat = message;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error conectando a Twitch: " + e.Message);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        running = false;
+        if (twitchThread != null && twitchThread.IsAlive)
+            twitchThread.Abort();
+
+        if (writer != null) writer.Close();
+        if (reader != null) reader.Close();
+        if (twitchClient != null) twitchClient.Close();
+    }
+
+
+    public void SpawnNPC()
+    {
+        this.gameObject.transform.position = new Vector3(UnityEngine.Random.Range(-16, 17), UnityEngine.Random.Range(-3.5f, 10), 0);
+        Instantiate(prefabNPC, gameObject.transform.position, Quaternion.identity);
+    }
+}
