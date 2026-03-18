fd917ac73df8abccab72dc418cc7e011c678e0df
NoGoBo616 <noegoterrisborras@gmail.com>
2026-03-18 17:51:14 +0100

    Bug_Twitch_Leucocito

---
+++ Asteroid_Prueva/Assets/Scripts/Identificador_twitch.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Identificador_twitch : MonoBehaviour
+{
+    public Twitch_Spawn twitch;
+
+    private void OnEnable()
+    {
+        twitch = FindAnyObjectByType<Twitch_Spawn>();
+        twitch.channelInput = GetComponent<TMP_InputField>();
+    }
+
+    private void Update()
+    {
+        if (twitch != null)
+        {
+            twitch = FindAnyObjectByType<Twitch_Spawn>();
+            twitch.channelInput = GetComponent<TMP_InputField>();
+        }
+    }
+
+    public void Boton()
+    {
+        twitch.Iniciar();
+    }
+}
