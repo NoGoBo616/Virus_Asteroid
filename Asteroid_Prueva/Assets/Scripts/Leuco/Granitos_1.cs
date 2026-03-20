bea35047649a4844e06dc9c6173a12093bb48a1b
NoGoBo616 <noegoterrisborras@gmail.com>
2026-03-20 21:08:58 +0100

    bug de granitos

--- Asteroid_Prueva/Assets/Scripts/Leuco/Granitos_1.cs
+++ Asteroid_Prueva/Assets/Scripts/Leuco/Granitos_1.cs
@@ -5,19 +5,19 @@ public class Granitos_1 : MonoBehaviour
     public Leucocito padre;
     public GameObject[] granitos;
     public int live;
-    int ran1;
-    int ran2;
+    public int ran1;
+    public int ran2;
 
     private void OnEnable()
     {
         ran1 = Random.Range(0, 3);
         ran2 = Random.Range(0, 3);
-        if (ran2 == ran1) ran2 = Random.Range(0, 3);
-        if (ran2 != ran1)
+        while (ran2 == ran1)
         {
-            granitos[ran1].gameObject.SetActive(true);
-            granitos[ran2].gameObject.SetActive(true);
+            ran2 = Random.Range(0, 3);
         }
+        granitos[ran1].gameObject.SetActive(true);
+        granitos[ran2].gameObject.SetActive(true);
         live = 2;
         this.gameObject.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 180));
     }
