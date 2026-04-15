fd511efc6e65788b12f49da46e33ab1aa54d9ee3
NoGoBo616 <noegoterrisborras@gmail.com>
2026-04-15 19:50:05 +0200

    ameba 3

--- Asteroid_Prueva/Assets/Scripts/Player/AmebaControl.cs
+++ Asteroid_Prueva/Assets/Scripts/Player/AmebaControl.cs
@@ -1,32 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AmebaControl : MonoBehaviour
 {
     public float lifeAmeba;
     public float timeAmeba;
-    public Rigidbody2D rbAmeba;
+    public Rigidbody2D rb;
     public bool flying;
     [SerializeField] float speed;
     public Vector3 destino;
     public float[] times;
-    private object rb;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-       
-        
-    }
     private void OnEnable()
     {
         lifeAmeba = 1f;
         timeAmeba =times[Random.Range(0,7)];
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         if (timeAmeba > 0)
         {
             timeAmeba -= Time.deltaTime;
@@ -36,23 +29,25 @@ public class AmebaControl : MonoBehaviour
         {
             timeAmeba = 0;
         }
-
     }
     
-
     private void FixedUpdate()
     {
        MoveAmeba();
-
     }
+
     void MoveAmeba()
     {
-        //rb.velocity= (Vector2)(destino - transform.position).normalized * speed;
+        rb.linearVelocity = (Vector2)(destino - transform.position).normalized * speed;
 
         if (Vector2.Distance(transform.position, destino) <= 0.1f)
         {
-            //rb.velocity = Vector2.zero;
-            Debug.Log("llegue");
+            
         }
+    }
+
+    void ChangeDestiny()
+    {
+
     }
 }
