9e741cd4fbe71ce5a11b2004d2f7ff3bf51fa945
NoGoBo616 <noegoterrisborras@gmail.com>
2026-05-21 12:49:55 +0200

    correccion de bugs y añadidos

--- Asteroid_Prueva/Assets/Scripts/Player/Shield.cs
+++ Asteroid_Prueva/Assets/Scripts/Player/Shield.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    public GolpeCinemachine shake;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         // Verifica si el objeto golpeado es un clon (hijo del asteroide)
-        if (other.CompareTag("AsteroidClone"))
+        if (other.CompareTag("AsteroidClone") || other.CompareTag("Flora") || other.CompareTag("Asteroid") || other.CompareTag("Police"))
         {
+            shake.Sehekear();
             Destroy(other.gameObject);
         }
     }
