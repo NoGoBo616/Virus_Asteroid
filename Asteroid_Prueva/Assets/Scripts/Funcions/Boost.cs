803185c3f2f6ff572b00f365289e57965b4555f6
NoGoBo616 <noegoterrisborras@gmail.com>
2026-05-25 01:06:47 +0200

    venas boost

--- Asteroid_Prueva/Assets/Scripts/Funcions/Boost.cs
+++ Asteroid_Prueva/Assets/Scripts/Funcions/Boost.cs
@@ -6,10 +6,14 @@ public class Boost : MonoBehaviour
     public float speed;
     public Player_ player;
     public GolpeCinemachine shake;
+    public GameObject[] animaciones; 
 
     private void OnEnable()
     {
         player = FindAnyObjectByType<Player_>();
+        this.gameObject.transform.position = new Vector2(Random.Range(-20, 21), Random.Range(-10, 11));
+        this.gameObject.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 90f));
+        animaciones[Random.Range(0,3)].gameObject.SetActive(true);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
